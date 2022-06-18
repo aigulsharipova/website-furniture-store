db0507dbda31714dfa7a0591d81b84bef145b112
Aigul <sharipova.aig@gmail.com>
2022-06-18 16:09:55 -0400

    function remove

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const items = document.querySelectorAll('.item');
-console.log(items);
 
 items.forEach(item => {
   item.addEventListener('mouseover', () => {
-    console.log("Clicked!!!")
-  })
+  item.classList.add('selected');
+  removeFocus();
+})
 })
