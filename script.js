3a68ffc8cbb66b6e8d55911902db2d5b19f3cd1a
Aigul <sharipova.aig@gmail.com>
2022-07-08 21:52:43 -0400

    ms in Hours, Min

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function openDateTimer() {
-  const openDate = new Date('July 15, 2022, 11:00');
+  const openDate = new Date('July 8, 2022, 20:29');
   const now = new Date();
   const diff = openDate - now;
 
   const msInDay = 24 * 60 * 60 * 1000;
   const displayDay = Math.floor(diff/msInDay);
   document.querySelector('.days').textContent = displayDay;
+  
+  const displayHours = Math.floor((diff%msInDay) / msInHour);
+  document.querySelector('.hours').textContent = displayHours;
+
+  const displayMin = Math.floor((diff%msInHour) / msInMinute);
+  document.querySelector('.minutes').textContent = displayMin;
 
   if (diff <= 0) {
     document.querySelector('.days').textContent = 0;
@@ -17,7 +23,9 @@ function openDateTimer() {
 let timerId = setInterval(openDateTimer, 1000);
 
 function startDate() {
-  const heading = document.querySelector('h3');
+  const heading = document.querySelector('.heading');
+  heading.textContent = "WE ARE FINALLY OPEN!!!";
+  heading.classList.add('.red');
 }
 
 const items = document.querySelectorAll('.item');
