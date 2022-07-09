28e5d5d946df526261acc2077ba8f68135f0639f
Aigul <sharipova.aig@gmail.com>
2022-07-08 20:03:18 -0400

    ms in day

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function openDateTimer() {
   const openDate = new Date('July 15, 2022, 11:00');
   const now = new Date();
   const diff = openDate - now;
+
+  const msInDay = 24 * 60 * 60 * 1000;
 }
 
 const items = document.querySelectorAll('.item');
